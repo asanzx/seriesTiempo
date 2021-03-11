170855637c525d54fc13299dfead8c1a1a6c1077
asanz20 <c-andres.sanz@payscale.com>
2021-03-11 20:05:44 -0300

    correccion del forward sliding

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Proyecto 3 de la carrera Data Science (Acamica).
 
 Proyecto abierto a comentarios.
+
+Nota 03.11.2021: Ya puede utilizarse tanto el sliding windows (sliding=True) como el forward chaining (sliding=False)
--- ts_split.py
+++ ts_split.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import pandas as pd
 
@@ -8,21 +9,25 @@ def ts_split(X, y, cv=4, test_split=0.3, sliding=False):
     b: 0-Train / 1-Test
     c: 0-X / 1-y
     '''
+    test_range = int(X.shape[0]*test_split)
+    cv_temp = cv
     train_test_df_dict = {}
-    temp = 0
+    temp = 0 
     
-    for i in range(cv):
-        group = (i+1)*int(X.shape[0]/cv)
-        group_split = int(group*(1 - test_split))
-        train_df = [X[:group_split,:], y[:group_split]]
-        test_df = [X[group_split:group,:], y[group_split:group]]
-        
-        if i == cv-1:
+    if not sliding:
+        cv_temp = math.floor(X.shape[0]/test_range) - 1
+        temp = cv_temp
+        print('El dataset no puede dividirse en {} partes, se procede a separar en {}'.format(cv, cv_temp))
+    
+        for i in range(cv_temp):
+            group = X.shape[0] - (test_range*i)
+            group_split = X.shape[0] - (test_range*(i+1))
             train_df = [X[:group_split,:], y[:group_split]]
-            test_df = [X[group_split:X.shape[0],:], y[group_split:X.shape[0]]]
+            test_df = [X[group_split:group,:], y[group_split:group]]
         
-        train_test_df_dict[i+1] = [train_df, test_df]
-    
+            train_test_df_dict[temp] = [train_df, test_df]
+            temp -= 1
+        
     if sliding:
         for i in range(cv):
             group = (i+1)*int(X.shape[0]/cv)
